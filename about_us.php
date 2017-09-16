4850e09da5ef8a3516eb86e8a164cc57b7669f8f
Jessica <hien.nguyen@singalarity.com>
2017-09-17 02:58:07 +0700

    Add process page + images

---
+++ about_us.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DDKaffee-About-us</title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+    <!-- Plugin CSS -->
+    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/creative.css" rel="stylesheet">
+    <link href="css/home.css" rel="stylesheet">
+    <link href="css/slider.css" rel="stylesheet">
+    <link href="css/slider_custom.css" rel="stylesheet">
+    <link href="css/about_us.css" rel="stylesheet">
+</head>
+<body>
+<!--Banner-->
+<div class="banner">
+<!-- Navigation-->
+
+    <div class="navbar-light" id="">
+      <div class="container">
+        <a class="navbar-left navbar-brand js-scroll-trigger bg-logo" href="#page-top"><img src="img/logo.png" alt="One of the best themes ever"></a>
+       
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="nav navbar-nav ml-auto navbar-right ">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#about">TRANG CHỦ</a>
+            </li>
+            <li class="nav-item active1">
+              <a class="nav-link js-scroll-trigger" href="#services">DOANH NGHIỆP</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#portfolio">BLOCK</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact">LIÊN HỆ</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    
+
+    <div class="container main-tittle">
+    	<h1>VỀ CHÚNG TÔI</h1>
+    </div>
+ </div>
+ <!--//banner-->
+    <div class="container"></div>
+    <div class="container">
+    <div class="process-page">
+    	<h2 id="process-tittle">QUY TRÌNH SẢN XUẤT</h2>
+    	<div class="tabs-list">
+    		<ul class="nav nav-tabs process" style=" outline: none; border-bottom: 0;">
+						<li class="step active" id="step1"><a href="#Tab1" data-toggle="tab"  style="padding: 0 0;">
+							<img src="img/about_us/process-1.png" alt="step1" class ="images img-responsive">
+						<div class="overlay"></div>
+						</a></li>
+						<li class="step">
+							<a href="#Tab2" data-toggle="tab" aria-expanded="false">
+								<img src="img/about_us/process-2.png " alt="step2" class="images img-responsive">
+							</a></li>
+						<li class="step">
+							<a href="#Tab3" data-toggle="tab" aria-expanded="false">
+								<img src="img/about_us/process-3.png " alt="step3" class="images img-responsive">
+							</a></li>
+						<li class="step">
+							<a href="#Tab4" data-toggle="tab" aria-expanded="false">
+								<img src="img/about_us/process-4.png " alt="step4" class="images img-responsive">
+							</a></li>
+			</ul>
+    	</div>
+    	<div class="tab-content">
+						<div class="tab-pane active" id="Tab1">
+							<div class="w3-text">
+								<img src="img/about_us/pic-1.jpg" class="img-responsive" alt="" style="width:100%; margin-top: 25px;">
+								<h1 id="nongtrai">NÔNG TRẠI</h1>
+								<p>Hành trình cà phê của chúng tôi bắt đầu tại nông trại D&D Kaffee</p>
+								<p>Nông trại được thiết lập theo tiêu chuẩn riêng biệt:</p>
+								<div class="container text-center" style="padding: 0;">    
+								  
+								  <div class="row" style="margin-top:30px;">
+								    <div class="col-md-4 col-xs-12">        
+								       <img src="img/about_us/cay-cafe-con.jpg" class="img-responsive" style="width:100%" alt="Image">
+								       	<p>Sử dụng 100% phân bón hữu cơ, cam kết tuyệt đối không sử dụng thuốc hoá học.</p>
+								   	</div>
+								    <div class="col-md-4 col-xs-12"> 
+								   	<img src="img/about_us/thu-hoach.png" style="width:100%" alt="Image">        
+								      <p>Thu hoạch hái lựa hoàn toàn trực tiếp bằng tay những trái chín, độ cam kết lên đến trên 90%, không thu nhặt những trái khô rơi rụng dưới đất.</p>  
+								    </div>
+								    <div class="col-md-4 col-xs-12">
+								     <img src="img/about_us/discuss.png" style="width:100%" alt="Image">
+								        <p>Nghiên cứu, bảo vệ và phát triển những giống cây cà phê chất lượng.</p>
+								    </div>    
+								  </div>
+								  <p style="margin-top: 20px; padding: 0; text-align: left;">Chúng tôi hiểu rằng mỗi tách cà phê tuyệt vời đều gắn liền với công sức và sự tâm huyết của người nông dân. Vì vậy, các hộ nông dân liên kết cùng chúng tôi trong dự án "Cà phê tử tế” được hỗ trợ những chính sách ưu đãi về giá, kỹ thuật tiên tiến và tập huấn nông nghiệp thường xuyên.</p>
+								</div>
+								<br>
+							</div>
+						</div>
+						<div class="tab-pane" id="Tab2">
+							<div class="w3-text">
+								<img src="images/t2.jpg" class="img-responsive" alt="">
+							</div>
+						</div>
+						<div class="tab-pane" id="Tab3">
+							<div class="w3-text">
+								<img src="images/t3.jpg" class="img-responsive" alt="">
+							</div>
+						</div>
+						<div class="tab-pane" id="Tab4">
+							<div class="w3-text">
+								<img src="images/t1.jpg" class="img-responsive" alt="">
+							</div>
+						</div>
+						
+    </div>
+    </div>
+
+    <div class="footer" style="background: url(img/footer.jpg) no-repeat; height: 300px; margin: 0; padding: 0;">
+    	<div class="">
+    		<div class="row">
+    			<div class="col-md-4"></div>
+    			<div class="col-md-4"></div>
+    			<div class="col-md-4"></div>
+    		</div>
+    		<div class="row">
+    			<div class="copy-right">
+				<p>© 2016 D&D Roasters. All Rights Reserved.</p>
+			</div>
+    		</div>
+    	</div>
+    </div>
+ </body>
+</html>
