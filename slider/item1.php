59e81c881b8fca98ad8480ed09cfe19c77340b42
Avada <avada@vinova.sg>
2017-09-28 18:30:15 +0700

    fix font size

--- slider/item1.php
+++ slider/item1.php
@@ -5,7 +5,7 @@
         <div class="header-content">
           <div class="header-content-inner header-content-inner-custom">
             <!-- <div class="intro"> -->
-              <div class="business-name" style="color: #d9a535 !important; font-weight: bold; ">D&D KAFFEE</div>
+              <div class="business-name" style="color: #d9a535 !important; font-weight: bold; font-size: 20px;">D&D KAFFEE</div>
               <h1 class="home-title">Giải pháp kinh doanh</h1>
               <h1 class="home-title">cà phê toàn diện</h1>
             <!-- </div> -->
